f65db28837f9c8eb3295381754848d6288b55640
YuxuanLiuTier4Desktop <619684051@qq.com>
2026-04-20 09:44:19 +0900

    fix path
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>

--- patch.sh
+++ patch.sh
@@ -1,1 +1,1 @@
-cp -r qwenvl3/transformers_replace/models /home/ukenryu/.local/lib/python3.10/site-packages/transformers/
+cp -r qwenvl3/transformers_replace/models ~/.local/lib/python3.10/site-packages/transformers/
